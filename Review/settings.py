1a2c484549921b97fc1ec4fb14497b834669d74e
Masaya00 <masaya.asxce@icloud.com>
2020-12-15 20:56:19 +0900

    add herokusetting

--- Review/settings.py
+++ Review/settings.py
@@ -128,3 +128,20 @@ STATIC_DIRS = [
 LOGIN_URL = 'ReviewApp:login'
 LOGIN_REDIRECT_URL = 'ReviewApp:index'
 
+# Herokuへのデプロイのために追記
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+DEBUG = False
+
+try:
+    from .local_settings import*
+except ImportError:
+    pass
+
--- Review/wsgi.py
+++ Review/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Review.settings')
 
 application = get_wsgi_application()
+# Herokuデプロイのデプロイのために追記
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
